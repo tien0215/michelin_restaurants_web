b042429d7734f9abdb8cc5fe1f8927ea113bf991
Tien hui Hsieh <grace86215@TiendeMacBook-Air.local>
2024-11-22 17:13:27 -0700

    Add login and register components

--- client/src/components/nav-component.js
+++ client/src/components/nav-component.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import AuthService from "../services/auth.service";
 
 const NavComponent = ({ currentUser, setCurrentUser }) => {
+  const location = useLocation();
+
+  // Specify pages where the search bar should not appear
+  const noSearchBarPages = ["/login", "/register"];
+  const shouldShowSearchBar = !noSearchBarPages.includes(location.pathname);
+
   const handleLogout = () => {
     AuthService.logout(); // 清空local storage
     window.alert("登出成功!現在您會被導向到首頁。");
@@ -14,22 +20,28 @@ const NavComponent = ({ currentUser, setCurrentUser }) => {
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
           {/* Search Section */}
-          <div className="d-flex align-items-center w-50 ms-3 mt-4">
-            <input
-              type="text"
-              className="form-control py-3 me-3 w-100 rounded-0"
-              placeholder="Type restaurant name here..."
-              aria-label="Type restaurant name here"
-              style={{ backgroundColor: "#D9D9D9", height: "50px" }}
-            />
-            <button
-              className="btn btn-danger rounded"
-              type="button"
-              style={{ height: "50px", fontFamily: "Inter", fontSize: "18px" }}
-            >
-              Search
-            </button>
-          </div>
+          {shouldShowSearchBar && (
+            <div className="d-flex align-items-center w-50 ms-3 mt-4">
+              <input
+                type="text"
+                className="form-control py-3 me-3 w-100 rounded-0"
+                placeholder="Type restaurant name here..."
+                aria-label="Type restaurant name here"
+                style={{ backgroundColor: "#D9D9D9", height: "50px" }}
+              />
+              <button
+                className="btn btn-danger rounded"
+                type="button"
+                style={{
+                  height: "50px",
+                  fontFamily: "Inter",
+                  fontSize: "18px",
+                }}
+              >
+                Search
+              </button>
+            </div>
+          )}
 
           {/* Navbar Toggle (for smaller screens) */}
           <button
