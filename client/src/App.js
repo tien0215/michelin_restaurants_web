814b290967fe5f603e798972719913041010b230
Tien hui Hsieh <grace86215@TiendeMacBook-Air.local>
2024-12-01 23:48:21 -0700

    Update dependencies, assets, and core files for app enhancements

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import Layout from "./components/Layout";
 import HomeComponent from "./components/home-component";
 import RegisterComponent from "./components/register-component";
 import LoginComponent from "./components/login-component";
+import RestaurantComponent from "./components/restaurant-component";
 import AuthService from "./services/auth.service";
-
+import ProfileComponent from "./components/profile-component";
 function App() {
   let [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
   return (
@@ -18,11 +19,39 @@ function App() {
           }
         >
           <Route index element={<HomeComponent />} />
+          <Route path="/homepage" element={<HomeComponent />} />
           <Route path="register" element={<RegisterComponent />} />
           <Route
             path="login"
             element={
               <LoginComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />{" "}
+          <Route
+            path="profile"
+            element={
+              <ProfileComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+          <Route
+            path="/api/restaurants/:id"
+            element={
+              <RestaurantComponent
+                currentUser={currentUser}
+                setCurrentUser={setCurrentUser}
+              />
+            }
+          />
+          <Route
+            path="/api/restaurants/findByName/:theName"
+            element={
+              <RestaurantComponent
                 currentUser={currentUser}
                 setCurrentUser={setCurrentUser}
               />
